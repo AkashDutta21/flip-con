191bb2bf801a14e9a1544b386f38c363c8015eeb
Akash Dutta <Akash.Dutta2@mindtree.com>
2021-09-11 12:01:48 +0530

    Initial Commit

---
+++ src/main/java/com/example/flipconservice/FlipconServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.flipconservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipconServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipconServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flipconservice/FlipconServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flipconservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipconServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
